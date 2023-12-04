1b9e6a71cd088acc24056945d6bcbf74b5cff9ac
NITISH KUMAR GIRI <123797264+nitishkumargiri@users.noreply.github.com>
2023-12-04 22:41:33 +0530

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:3001';
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-export const getRestaurantData = () => api.get('/restaurant').then((response) => response.data);
